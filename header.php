eba6c51ad0dd6dd690d481377b1f618825ac14a1
sebastianmarco55 <104759331+sebastianmarco55@users.noreply.github.com>
2024-03-13 14:16:05 +0200

    V20

---
+++ header.php
@@ -0,0 +1,43 @@
+    <!-- Bară de Navigare Început -->
+    <nav
+      class="navbar navbar-expand-lg bg-white navbar-light shadow-sm py-3 py-lg-0 px-3 px-lg-0"
+    >
+      <a href="index.html" class="navbar-brand ms-lg-5">
+        <h1 class="display-5 m-0 text-primary2">
+          <img
+            src="img/favicon2.png"
+            alt=""
+            style="
+              width: 60%;
+              border-radius: 10px;
+              background-color: rgb(21, 59, 95);
+              padding-left: 10px;
+              padding-right: 10px;
+            "
+          />
+        </h1>
+      </a>
+      <button
+        class="navbar-toggler"
+        type="button"
+        data-bs-toggle="collapse"
+        data-bs-target="#navbarCollapse"
+      >
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarCollapse">
+        <div class="navbar-nav ms-auto py-0">
+          <a href="index.html" class="nav-item nav-link">Acasă</a>
+          <a href="about.html" class="nav-item nav-link active">Despre noi</a>
+          <a href="service.html" class="nav-item nav-link">Servicii</a>
+          <a href="career.html" class="nav-item nav-link">Cariera</a>
+          <a href="contact.html" class="nav-item nav-link">Contact</a>
+          <a
+            href="tel:+123456789"
+            class="nav-item nav-link nav-contact bg-secondary text-white px-5 ms-lg-5"
+            ><i class="bi bi-telephone-outbound me-2"></i>+40 731 200 520</a
+          >
+        </div>
+      </div>
+    </nav>
+    <!-- Bară de Navigare Sfârșit -->
